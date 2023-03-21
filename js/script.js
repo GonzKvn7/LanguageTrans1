8c7b58e39657fe4a411e949a4acaf7576e2b4dc6
GonzKvn7 <GonzKvn7@proton.me>
2023-03-21 16:19:50 -0700

    API added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
-const selectTag = document.querySelectorAll("select");
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+selectTag = document.querySelectorAll("select"),
+translateBtn = document.querySelector("button");
 
 selectTag.forEach((tag,id) => {
     for (const country_code in countries) {
+        // Selecting English by default as FROM language and Indonesian as TO Language
         let selected;
         if(id == 1 && country_code == "en-GB") {
             selected = "selected";
-        } else if (id == 1 && country_code == "hi-IN") {
+        } else if (id == 1 && country_code == "id-ID") {
             selected = "selected";
         }
-        let option = `<option value="${country_code}">${countries[country_code]}</option>`;
+        let option = `<option value="${country_code}"${selected}>${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option); // adding options tag inside select tag
     }
+});
+
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value,
+    translateFrom = selectTag[0].value, // getting fromSelect tag value
+    translateTo = selectTag[1].value; // getting toSelect tag value 
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    // fetching api response and returning it with parsing into js obj
+    //and in another then method receiving that obj
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        console.log(data);
+        toText.value = data.responseData.translatedText;
+    });
 });
