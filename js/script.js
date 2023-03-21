ba5f32759ecb6f070b5b08f75c8dfbaf3d93adeb
GonzKvn7 <GonzKvn7@proton.me>
2023-03-20 19:33:49 -0700

    Corrected some faulty code and added a js folder

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const selectTag = document.querySelectorAll("select");
+
+selectTag.forEach((tag,id) => {
+    for (const country_code in countries) {
+        let selected;
+        if(id == 1 && country_code == "en-GB") {
+            selected = "selected";
+        } else if (id == 1 && country_code == "hi-IN") {
+            selected = "selected";
+        }
+        let option = `<option value="${country_code}">${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option); // adding options tag inside select tag
+    }
+});
