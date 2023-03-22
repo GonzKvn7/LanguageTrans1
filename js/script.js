53eb8cf5d8daf84a04fdf76f3049eb15f4858d98
GonzKvn7 <GonzKvn7@proton.me>
2023-03-21 20:22:21 -0700

    Complete. Will adjust some problems

--- js/script.js
+++ js/script.js
@@ -28,21 +28,21 @@ exchangeIcon.addEventListener("click", () => {
     selectTag[0].value = selectTag[1].value;
     toText.value = tempText;
     selectTag[1].value = tempLang;
-
-
-})
-
+});
 
 translateBtn.addEventListener("click", () => {
     let text = fromText.value,
     translateFrom = selectTag[0].value, // getting fromSelect tag value
     translateTo = selectTag[1].value; // getting toSelect tag value 
+    if(!text) return
+    toText.setAttribute("placeholder", "Translating...");
     let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
     // fetching api response and returning it with parsing into js obj
     //and in another then method receiving that obj
     fetch(apiUrl).then(res => res.json()).then(data => {
         console.log(data);
         toText.value = data.responseData.translatedText;
+        toText.setAttribute("placeholder", "Translation");
     });
 });
 
